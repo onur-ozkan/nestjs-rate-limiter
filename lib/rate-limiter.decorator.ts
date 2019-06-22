3c5a0b32978db0b6d13a3f18b81f32fd1b1f2421
Ryan Dowling <ryan@ryandowling.me>
2019-06-22 22:54:06 +1000

    fix: the decorator not showing the options correctly

--- lib/rate-limiter.decorator.ts
+++ lib/rate-limiter.decorator.ts
@@ -2,4 +2,4 @@ import { SetMetadata } from '@nestjs/common';
 
 import { RateLimiterModuleOptions } from './rate-limiter.interface';
 
-export const RateLimit: any = (options: RateLimiterModuleOptions) => SetMetadata('rateLimit', options);
+export const RateLimit = (options: RateLimiterModuleOptions): MethodDecorator => SetMetadata('rateLimit', options);
