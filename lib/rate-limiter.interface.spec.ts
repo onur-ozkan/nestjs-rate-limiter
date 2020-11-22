06f13eb8209f28996d643a422efd9bf4bc7de854
Shannon Lal <shannonlal@hotmail.com>
2020-11-22 13:44:35 -0500

    Adding some simple tests for validating interface

--- lib/rate-limiter.interface.spec.ts
+++ lib/rate-limiter.interface.spec.ts
@@ -1,4 +1,4 @@
-import { RateLimiterOptionsFactory, RateLimiterModuleAsyncOptions } from './rate-limiter.interface'
+import { RateLimiterOptionsFactory, RateLimiterModuleAsyncOptions, RateLimiterOptions } from './rate-limiter.interface'
 
 describe('RateLimiterOptionsFactory', () => {
 	it('should validate that RateLimiterOptionsFactory exists', async () => {
@@ -26,4 +26,28 @@ describe('RateLimiterModuleAsyncOptions', () => {
 		expect(rateLimiterModuleAsyncOptions.useFactory).toBeDefined()
 		expect(rateLimiterModuleAsyncOptions.inject.length).toBe(1)
 	})
-});
+})
+
+describe('RateLimiterOptions', () => {
+	it('should validate that RateLimiterOptions with no properties', async () => {
+		const rateLimiterOptions: RateLimiterOptions = {}
+		expect(rateLimiterOptions).toBeDefined()
+		expect(rateLimiterOptions.for).toBeUndefined()
+    })
+    
+    it('should validate that RateLimiterOptions with no properties', async () => {
+		const rateLimiterOptions: RateLimiterOptions = {
+            for: 'Express',
+            type: 'Memory',
+            points: 2,
+            pointsConsumed: 3,
+            dbName: 'test'
+        }
+		expect(rateLimiterOptions).toBeDefined()
+        expect(rateLimiterOptions.for).toBe('Express')
+        expect(rateLimiterOptions.type).toBe('Memory')
+        expect(rateLimiterOptions.points).toBe(2)
+        expect(rateLimiterOptions.pointsConsumed).toBe(3)
+        expect(rateLimiterOptions.dbName).toBe('test')
+	})
+})
