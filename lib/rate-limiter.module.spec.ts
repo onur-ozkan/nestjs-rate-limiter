c5bc594dd0f304fb9012f82aafd33e1ed7b52161
Shannon Lal <shannonlal@hotmail.com>
2020-11-24 19:31:05 -0500

    Addind test for async options

--- lib/rate-limiter.module.spec.ts
+++ lib/rate-limiter.module.spec.ts
@@ -1,54 +1,53 @@
 import { DynamicModule, Provider } from '@nestjs/common'
-import { RateLimiterModule } from './rate-limiter.module';
-import { RateLimiterOptions, RateLimiterModuleAsyncOptions } from './rate-limiter.interface';
+import { RateLimiterModule } from './rate-limiter.module'
+import { RateLimiterOptions, RateLimiterModuleAsyncOptions } from './rate-limiter.interface'
 
 describe('RateLimiterModule', () => {
 	it('should validate that RateLimiterModule exists', async () => {
 		expect(RateLimiterModule).toBeDefined()
-    })
-    
-    it('should register RateLimiterModule with empty options', async () => {
-        const rateLimiterOptions: RateLimiterOptions = {};
-        
-        const registeredDynamicModule: DynamicModule = RateLimiterModule.register(rateLimiterOptions);
+	})
 
-        expect( registeredDynamicModule ).toBeDefined();
-        expect( typeof registeredDynamicModule.module).toBeDefined();
-        expect( registeredDynamicModule.providers.length).toBe(1);
-        const rateLimitOptionsProvider: Provider<any> = registeredDynamicModule.providers[0];
-        expect( rateLimitOptionsProvider['provide'] ).toBe('RATE_LIMITER_OPTIONS');
-        expect( rateLimitOptionsProvider['useValue'] ).toBeDefined();
-    })
-    
-    it('should register RateLimiterModule with default options', async () => {
-        const rateLimiterOptions: RateLimiterOptions = {};
-        
-        const registeredDynamicModule: DynamicModule = RateLimiterModule.register();
+	it('should register RateLimiterModule with empty options', async () => {
+		const rateLimiterOptions: RateLimiterOptions = {}
 
-        expect( registeredDynamicModule ).toBeDefined();
-        expect( typeof registeredDynamicModule.module).toBeDefined();
-        expect( registeredDynamicModule.providers.length).toBe(1);
-        const rateLimitOptionsProvider: Provider<any> = registeredDynamicModule.providers[0];
-        expect( rateLimitOptionsProvider['provide'] ).toBe('RATE_LIMITER_OPTIONS');
-        expect( rateLimitOptionsProvider['useValue'] ).toBeDefined();
+		const registeredDynamicModule: DynamicModule = RateLimiterModule.register(rateLimiterOptions)
 
-        const options: RateLimiterOptions = rateLimitOptionsProvider['useValue'];
+		expect(registeredDynamicModule).toBeDefined()
+		expect(typeof registeredDynamicModule.module).toBeDefined()
+		expect(registeredDynamicModule.providers.length).toBe(1)
+		const rateLimitOptionsProvider: Provider<any> = registeredDynamicModule.providers[0]
+		expect(rateLimitOptionsProvider['provide']).toBe('RATE_LIMITER_OPTIONS')
+		expect(rateLimitOptionsProvider['useValue']).toBeDefined()
+	})
 
-        expect( options.for ).toBe('Express');
-    })
-    
-    it('should register async RateLimiterModule with async options', async () => {
-        const rateLimiterOptions: RateLimiterModuleAsyncOptions = {};
-        
-        const registeredDynamicModule: DynamicModule = RateLimiterModule.registerAsync(rateLimiterOptions);
+	it('should register RateLimiterModule with default options', async () => {
+		const rateLimiterOptions: RateLimiterOptions = {}
 
-        console.log( 'Register dynamic module',registeredDynamicModule );
-        expect( registeredDynamicModule ).toBeDefined();
-        expect( typeof registeredDynamicModule.module).toBeDefined();
-        expect( registeredDynamicModule.providers.length).toBe(2);
-        const rateLimitOptionsProvider: Provider<any> = registeredDynamicModule.providers[0];
-        expect( rateLimitOptionsProvider['provide'] ).toBe('RATE_LIMITER_OPTIONS');
-        expect( rateLimitOptionsProvider['useValue'] ).toBeDefined();
-    })
+		const registeredDynamicModule: DynamicModule = RateLimiterModule.register()
 
+		expect(registeredDynamicModule).toBeDefined()
+		expect(typeof registeredDynamicModule.module).toBeDefined()
+		expect(registeredDynamicModule.providers.length).toBe(1)
+		const rateLimitOptionsProvider: Provider<any> = registeredDynamicModule.providers[0]
+		expect(rateLimitOptionsProvider['provide']).toBe('RATE_LIMITER_OPTIONS')
+		expect(rateLimitOptionsProvider['useValue']).toBeDefined()
+
+		const options: RateLimiterOptions = rateLimitOptionsProvider['useValue']
+
+		expect(options.for).toBe('Express')
+	})
+
+	it('should register async RateLimiterModule with async options', async () => {
+		const rateLimiterOptions: RateLimiterModuleAsyncOptions = {}
+
+		const registeredDynamicModule: DynamicModule = RateLimiterModule.registerAsync(rateLimiterOptions)
+
+		expect(registeredDynamicModule).toBeDefined()
+		expect(typeof registeredDynamicModule.module).toBeDefined()
+		expect(registeredDynamicModule.providers.length).toBe(2)
+		const rateLimitOptionsProvider: Provider<any> = registeredDynamicModule.providers[0]
+		expect(rateLimitOptionsProvider['provide']).toBe('RATE_LIMITER_OPTIONS')
+		expect(rateLimitOptionsProvider['useFactory']).toBeDefined()
+		expect(rateLimitOptionsProvider['inject']).toBeDefined()
+	})
 })
