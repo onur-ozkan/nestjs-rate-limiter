07e195bec9756d2cfe6622e90f73156e673a7719
Shannon Lal <shannonlal@hotmail.com>
2020-12-12 14:23:15 -0500

    Removing unused test cases

--- examples/libs/rate-limiter-points-test/src/lib/rate-limiter-points-test.spec.ts
+++
@@ -1,7 +0,0 @@
-import { rateLimiterPointsTest } from './rate-limiter-points-test';
-
-describe('rateLimiterPointsTest', () => {
-  it('should work', () => {
-    expect(rateLimiterPointsTest()).toEqual('rate-limiter-points-test');
-  });
-});
