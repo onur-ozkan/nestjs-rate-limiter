a6bc789cf55f2498370ff99a61c067b58892f69a
Shannon Lal <shannonlal@hotmail.com>
2020-12-22 13:22:41 -0500

    Integrated controller exec evently

--- examples/apps/rate-limiter-express-app/src/app/app.module.ts
+++ examples/apps/rate-limiter-express-app/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { AppController, PointsController, BlackWhiteController } from './controllers';
+import { 
+  AppController, 
+  PointsController, 
+  BlackWhiteController, 
+  ExecEvenlyController } from './controllers';
 import { AppService } from './services/app.service';
 import { RateLimiterModule, RateLimiterInterceptor } from '../../../../../dist';
 
 @Module({
   imports: [RateLimiterModule],
-  controllers: [AppController, PointsController, BlackWhiteController],
+  controllers: [ 
+    AppController,
+    PointsController,
+    BlackWhiteController,
+    ExecEvenlyController],
   providers: [AppService,
     {
       provide: APP_INTERCEPTOR,
--- examples/apps/rate-limiter-express-app/src/app/controllers/index.ts
+++ examples/apps/rate-limiter-express-app/src/app/controllers/index.ts
@@ -1,3 +1,4 @@
 export { AppController } from './app.controller';
 export { PointsController} from './points.controller';
 export { BlackWhiteController } from './blackwhite.controller';
+export { ExecEvenlyController } from './exec.controller';
